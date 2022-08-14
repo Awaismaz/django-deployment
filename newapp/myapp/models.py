f5c4aa6fd692dc395614a0473831ee4357a69a15
Awais Mazahir <awais_mazahir@hotmail.com>
2022-08-14 16:06:06 +0500

    'first'

---
+++ newapp/myapp/models.py
@@ -0,0 +1,19 @@
+from tkinter import CASCADE
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class UserProfileInfo(models.Model):
+
+    # Create relationship (don't inherit from User!)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    # Add any additional attributes you want
+    portfolio = models.URLField(blank=True)
+    # pip install pillow to use this!
+    # Optional: pip install pillow --global-option=”build_ext” --global-option=”--disable-jpeg”
+    pic = models.ImageField(upload_to='profile_pics',blank=True)
+
+    def __str__(self):
+        # Built-in attribute of django.contrib.auth.models.User !
+        return self.user.username
